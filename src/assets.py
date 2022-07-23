9520cc15c2a1026c2721d782cd7158ed063f133a
Viktor Modroczky <v.modroczky@gmail.com>
2022-07-23 20:47:24 +0200

    added versions to names

--- src/assets.py
+++ src/assets.py
@@ -8,7 +8,7 @@ bundles = {
         filters='jsmin'
     ),
     'global_css': Bundle(
-        'css/material-icons-swap.css',
+        'css/material-icons-v135-swap.css',
         'css/materialize-1.1.0.min.css',
         output='css/gen/global.css',
         filters='cssmin'
@@ -17,15 +17,15 @@ bundles = {
         'js/jquery-ui-1.13.2.bounce-only.js',
         'js/ScrollMagic-2.0.7.min.js',
         'js/leaflet-1.8.0.js',
-        'js/Control.FullScreen.js',
-        'js/L.Control.ResetView.min.js',
+        'js/Leaflet.Fullscreen-2.4.0.js',
+        'js/Leaflet.ResetView-1.1.1.min.js',
         output='js/gen/index.js',
         filters='jsmin'
     ),
     'index_css': Bundle(
         'css/leaflet-1.8.0.css',
-        'css/Control.FullScreen.css',
-        'css/L.Control.ResetView.min.css',
+        'css/Leaflet.Fullscreen-2.4.0.css',
+        'css/Leaflet.ResetView-1.1.1.min.css',
         output='css/gen/index.css',
         filters='cssmin'
     )
