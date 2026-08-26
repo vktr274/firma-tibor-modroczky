c3dd8857ac84cb918f44f4e8626072f835fa67cb
Viktor Modroczký <viktor.modroczky@aspecta.sk>
2026-08-26 19:33:23 +0200

    Change domain

--- src/freezer.py
+++ src/freezer.py
@@ -1,7 +1,7 @@
 from flask_frozen import Freezer
 from app import app, assets
 
-app.config["FREEZER_BASE_URL"] = "https://modroczky.eu.pythonanywhere.com/"
+app.config["FREEZER_BASE_URL"] = "https://modroczky.sk/"
 app.config["FREEZER_DESTINATION"] = "build"
 app.config["FREEZER_RELATIVE_URLS"] = True
 assets.url = "static"
